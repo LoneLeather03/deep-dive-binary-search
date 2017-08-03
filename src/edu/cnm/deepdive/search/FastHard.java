5a42880bc9057c64adbc09a0861b645d16aaefb6
LoneLeather03 <davidmartinez333@gmail.com>
2017-08-03 09:27:03 -0600

    Made more efficient.

--- src/edu/cnm/deepdive/search/FastHard.java
+++ src/edu/cnm/deepdive/search/FastHard.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.cnm.deepdive.search;
 
 import java.io.BufferedReader;
@@ -12,16 +9,9 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-/**
- * @author David S. Martinez
- *
- */
-public class BruteForce {
-
-	/**
-	 * @param args
-	 */
+public class FastHard {
 	private static String haystackName;
 	private static String needlesName;
 	private static String outputName;
@@ -34,11 +24,16 @@ public class BruteForce {
 		long startTime = System.currentTimeMillis();
 		ArrayList<Integer> haystack = readFile(haystackName);
 		ArrayList<Integer> needles = readFile(needlesName);
-			System.out.printf("Read data: %,d ms%n",
+		System.out.printf("Read data: %,d ms%n",
 		startTime =	System.currentTimeMillis()- startTime);
+			
+		quickSort(haystack);
+		System.out.printf("Sort data: %,d ms%n",
+		System.currentTimeMillis()- startTime);
+			
 		List<Integer> found = getNeedlesFound(haystack, needles);
-			System.out.printf("Read data: %,d ms%n",
-				System.currentTimeMillis()- startTime);
+		System.out.printf("Read data: %,d ms%n",
+		System.currentTimeMillis()- startTime);
 		writeNeedlesFound(outputName, found);
 	}
 	
@@ -64,14 +59,12 @@ public class BruteForce {
 	}
 	
 	private static List<Integer> getNeedlesFound(
-			List<Integer> haystack, List<Integer> needles) {
+			ArrayList<Integer> haystack, List<Integer> needles) {
 		ArrayList<Integer> results = new ArrayList<>();
-		for (int needle : needles) {
-			for(int haystalk : haystack) {
-				if (needle == haystalk) {
+		for (Integer needle : needles) {
+			int position = binarySearch(haystack, needle);
+				if (position >= 0) {
 					results.add(needle);
-					break;
-				}
 			}
 		}
 		return results;
@@ -92,5 +85,48 @@ public class BruteForce {
 			throw new RuntimeException(ex);
 		}
 	}
+	private static void quickSort(ArrayList<Integer> data) {
+		Random rng = new Random();
+		quickSort(data, 0, data.size(), rng);
+	}
 	
+	private static void quickSort(ArrayList<Integer> data,
+								  int start, int end, Random rng) {
+		if (end - start > 1) {
+			int pivotPosition = start + rng.nextInt(end - start);
+			int partitionPosition = start;
+			Integer pivot = data.get(pivotPosition);
+			data.set(pivotPosition, data.get(start));
+			data.set(start, pivot);
+			for (int i = start + 1; i < end; i++) {
+				Integer test = data.get(i);
+				if (pivot.compareTo(test) > 0) {
+					Integer temp = data.get(++partitionPosition);
+					data.set(partitionPosition, data.get(i));
+					data.set(i, temp);
+				}
+			}
+			data.set(start, data.get(partitionPosition));
+			data.set(partitionPosition, pivot);
+			quickSort(data, start, partitionPosition, rng);
+			quickSort(data, partitionPosition + 1, end, rng);
+		}
+	}
+	private static int binarySearch(ArrayList<Integer> data, Integer value) {
+		return binarySearch(data, value, 0, data.size());
+	}
+	private static int binarySearch(ArrayList<Integer> data, Integer value,
+									int start, int end) {
+		int midpoint = (start + end) / 2;
+		int comparison = data.get(midpoint).compareTo(value);
+		if (comparison == 0) {
+			return midpoint;
+		} else if (comparison < 0) {
+			return (end - midpoint > 1) ? binarySearch(data, value, midpoint + 1, end)
+					: -(end + 1);
+		} else {
+			return (midpoint > start) ? binarySearch(data, value, start, midpoint)
+					: -(midpoint + 1);
+		}
+	}
 }
